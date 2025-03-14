a47cb4b43e884e5af96da2ff574a60563f1c21fa
AdrianaS13 <adriana.msalas13@gmail.com>
2025-03-14 14:22:08 +0100

    Revert "buttonAwards"
    This reverts commit bc1b90b99f9185085c8b02515d9eff96320e6e17.

--- Assets/Scripts/LocationValidator.cs
+++ Assets/Scripts/LocationValidator.cs
@@ -25,7 +25,6 @@ public class LocationValidator : MonoBehaviour
 
     private void Start()
     {
-        buttonTakePicture.enabled = true;
         StartCoroutine(StartLocationServices());
     }
 
